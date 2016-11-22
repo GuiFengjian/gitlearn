192396063cbe56ddb90f612e4876d30f94cb9aa6
GuiFengjian <youxiang626@126.com>
2016-11-22 17:18:17 -0600

    second commit

--- dingshuibao/source/conf.py
+++ dingshuibao/source/conf.py
@@ -124,6 +124,7 @@ todo_include_todos = False
 #
 html_theme = 'default'
 
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
